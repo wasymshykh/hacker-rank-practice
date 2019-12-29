2c58ae24af4979cf1de8aaecf3658c71a84849c0
Wasym Shykh <wasymshykh@gmail.com>
2019-12-29 22:09:03 +0500

    hourglass small fix :(

--- hourglasses_2d_arry_practice.py
+++ hourglasses_2d_arry_practice.py
@@ -7,18 +7,15 @@ import math
 
 def print_other(o_arr, maximum):
     print("------")
-    flag = True
     m = 0
     for ii in range(0, len(o_arr)):
         for jj in range(0, len(o_arr[ii])):
             print(str(o_arr[ii][jj]) + " ", end="")
             if ii == 0 or ii == 2 or (ii == 1 and jj == 1):
                 m += o_arr[ii][jj]
-
         print("")
-    if flag:
-        if m > maximum:
-            return m
+    if m > maximum:
+        return m
     return max
 
 
